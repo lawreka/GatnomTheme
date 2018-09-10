a6576b5124a369c195def8f091960715c034820e
Kathryn Lawrence <lawr3ka@gmail.com>
2018-09-10 21:38:16 +0200

    MONTAG Issue 3 redesign
    updated navigation, mobile styles for navigation, logo and logotext, print editions, deleted footer, switched to light theme

--- assets/js/gatnom.js
+++ assets/js/gatnom.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
+  console.log("hi");
+  $("#issuesmenulink").click(function(){
+    $("#issuesmenu").toggleClass("hidden");
+  });
+  $("#hamburger-menu").click(function(){
+    $("#menu-container").toggleClass("hidden");
+  });
+  $("#antihamburger").click(function(){
+    $("#menu-container").toggleClass("hidden");
+  });
+  console.log("boo");
+});
+
+// print issues cover title reveal
+// $(".MONTAGcover").on("hover", function(){
+//
+// });
 
  // menu functionality
-  $("#hamburger-menu").on("click", function(){
-    $("#menu-container").toggleClass("hidden");
-    $("#homepageposts").toggleClass("hidden");
-    $(".pagination").toggleClass("hidden");
-    $("#footer").toggleClass("hidden");
-    return false;
-  });
 
-});
+    //
+    // return false;
